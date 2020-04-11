d921e56ad5d24dc9f426ce8d41b8db362359bc4e
Jung Lee <lee.jung.s95@gmail.com>
2020-04-11 17:20:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def turn_count(board)
   turn = 0
   board.each do |player|
@@ -14,4 +15,10 @@ def current_player(board)
   else
     "O"
   end
+=======
+def turn_count(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  turn_count.each do |board|
+  end
+  turn_count(board)
+>>>>>>> f7f835c35a72da696b425a3034261575e8d3e1b6
 end
